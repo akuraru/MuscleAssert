af48fde1dfc3c53cf9c8e87d282e6bc30cfb941c
akuraru <akuraru@gmail.com>
2016-12-17 11:36:08 +0900

    init

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+use_frameworks!
+
+target :Test do
+  pod "MuscleAssert", :path => "./"
+end
+
+target :Benchmark do
+end
---
+++ Test/MuscleAssertTests.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  MuscleAssert
+//
+//  Created by akuraru on 2016/12/17.
+//
+//
+
+import Foundation
+import XCTest
+
+class MuscleAssertTests : XCTestCase {
+    func testSample() {
+        XCTAssert(true)
+    }
+}
